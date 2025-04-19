e19a7f0815ea9dcc2aae52a0a1593ebadfcdc27c
ClPhillips10 <Lakephillips3232@gmail.com>
2025-04-19 11:42:00 -0500

    Project start

---
+++ schwifty.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void left(char word[]);
+void right(char word[]);
+void inc(char word[]);
+void dec(char word[]);
+
+int main() {
+
+   return 0;
+}
+
+void left(char word[]) {
+   
+}
+
+void right(char word[]) {
+   
+}
+
+void inc(char word[]) {
+   
+}
+
+void dec(char word[]) {
+   
+}
